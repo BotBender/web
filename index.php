cc2847e1239852ddc51005d4c31082368256d4f8
BotBender <Egor_ma_163@mail.ru>
2020-03-31 01:46:33 +0300

    first commit

--- index.php
+++ index.php
@@ -1,7 +1,14 @@
 <?
-include egor.php;
+$array = array(
+	'id' => '10903', 
+	'hid' => 'null', 
+	'full_name' => 'null', 
+	'address' => '420012, ., ., 1', 
+	'phone' => '543-51-55'
+);
+ 
+$json = json_encode($array);
+echo $json;
 
-////dada
-
-$item=json_class();
-echo $item->id;
+//$item=json_class();
+//echo $item->id;
