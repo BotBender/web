84e17f09b16fab3d18c1fbea57263cd2124f9ede
Wiktoplaz <pol.dast.vid@gmail.com>
2020-03-31 01:09:00 +0300

    dd

--- egor.php
+++ egor.php
@@ -1,3 +1,4 @@
+<?php
 $json = '{"id":"Значение 1","hid":"Значение 2","full_name":"Значение 3","address":"Значение 4","phone":"Значение 5"}';
 $array = json_decode($json, true);
 Array
--- index.php
+++ index.php
@@ -3,8 +3,23 @@ include egor.php;
 
 ////dada
 
-$item=json_class();
-echo $item->id;
-//добав
-json_item_add(id,hid,full_name,address,phone);
-json_item_update(id,hid,full_name,address,phone);
+
+//json_class();
+//выводит данные тип object из json-файла
+
+//class_json(object);
+//перезаписывает(удаляет файл LPU.json и создает новый. Записываем туда данные полученны при содирование из object в json)  
+
+
+
+//json_item_add(id,hid,full_name,address,phone);
+//создание ; 
+
+//редактирование;
+//json_item_update(id,hid,full_name,address,phone);
+
+//удаление необходимо 
+//json_item_del(id);
+
+//чтение будет реализовано напрямую на frontend
+
