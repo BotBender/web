32d0d7aa7bb152c642e6b351e1c470408d390393
Wiktoplaz <pol.dast.vid@gmail.com>
2020-03-31 02:07:37 +0300

    dd

--- index.php
+++ index.php
@@ -14,14 +14,20 @@ echo 'asd';
 
 
 //json_item_add(id,hid,full_name,address,phone);
-//добавленее json-элемента в файле LPU.json ; 
+//добавленее json-элемента в файле LPU.json ;
+//с помощью функкции json_class();
+//и процедуры class_json(object);
 
 //json_item_update(id,hid,full_name,address,phone);
 //редактирование json-элемента в файле LPU.json ;
+//с помощью функкции json_class();
+//и процедуры class_json(object);
  
 //json_item_del(id);
 //Удалить json-элемента в файле LPU.json ;
+//с помощью функкции json_class();
+//и процедуры class_json(object);
 
 //json_item_sel(bagin_i,end_i);
 //вывод json-элементов с bagin_i по end_i из файла LPU.json ;
-
+//с помощью функкции json_class();
