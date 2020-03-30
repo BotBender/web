42333353532122e85eca5482981de6fbc05a93aa
Wiktoplaz <pol.dast.vid@gmail.com>
2020-03-31 00:31:45 +0300

    dd

--- index.php
+++ index.php
@@ -1,21 +1,5 @@
 <?
-
-class item{
-	public $id;
-	public $hid;
-	public $full_name;
-	public $address;
-	public $phone;
-}
-
-$item= new item;
-$item->id='1';
-$item->hid='1';
-$item->full_name='1';
-$item->address='1';
-$item->phone='1';
-
-echo $item->id;
+include egor.php;
 
 ////dada
 
