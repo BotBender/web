c3af716e91cd7962383e4c4c8ad46fb94019b39c
BotBender <Egor_ma_163@mail.ru>
2020-03-31 02:07:51 +0300

    first commit

---
+++ egor.php
@@ -0,0 +1,11 @@
+$json = '{"id":"Значение 1","hid":"Значение 2","full_name":"Значение 3","address":"Значение 4","phone":"Значение 5"}';
+$array = json_decode($json, true);
+Array
+(
+    [id] => Значение 1
+    [hid] => Значение 2
+    [full_name] => Значение 3
+    [address] => Значение 4
+    [phone] => Значение 5
+)
+print_r($array);
